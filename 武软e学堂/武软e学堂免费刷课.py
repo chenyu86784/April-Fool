9fcd0b6fec3bcf8d5358f96e26d39ba8cbcc3e0e
chenyu86784 <62493561+chenyu86784@users.noreply.github.com>
2020-04-01 13:27:51 +0800

    first

---
+++ 武软e学堂/武软e学堂免费刷课.py
@@ -0,0 +1,8 @@
+import requests
+name=input("请输入e学堂用户名：")
+passwd=input("请输入密码：")
+url='http://wrggka.whvcse.edu.cn/api/M_User/Login?username='+name+'&password='+passwd+'&accessKey=1&secretKey=1'
+res = requests.get(url).json()
+user=res['trueName']
+print('你好'+user+'作业自己做')
+input('输入任意字符回车结束窗口')

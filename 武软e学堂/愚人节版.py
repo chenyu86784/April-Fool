c032e12af6a949c45bf2e8aacceed92f13f4be2e
chenyu86784 <62493561+chenyu86784@users.noreply.github.com>
2020-04-20 13:33:18 +0800

    愚人节版本 真实性优化
    我也搞不明白为什么我要更新这个版本。。。

--- 武软e学堂/愚人节版.py
+++ 武软e学堂/愚人节版.py
@@ -1,8 +1,10 @@
-import requests
+import requests,time
 name=input("请输入e学堂用户名：")
 passwd=input("请输入密码：")
 url='http://wrggka.whvcse.edu.cn/api/M_User/Login?username='+name+'&password='+passwd+'&accessKey=1&secretKey=1'
 res = requests.get(url).json()
 user=res['trueName']
+print('正在为您刷课......')
+time.sleep(10) 
 print('你好'+user+'，作业自己做')
 input('输入任意字符回车结束窗口')
